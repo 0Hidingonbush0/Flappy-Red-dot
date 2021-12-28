8b0fdcb1e2e321f628fe0d36241c329ed47819a2
David <davidgympl@gmail.com>
2021-12-28 20:27:51 +0100

    added some classes, especially gravity

--- flappybird.py
+++ flappybird.py
@@ -1,11 +1,11 @@
 import pygame as pg
 import sys
-
+import time
 #Základní parametry
 pg.init()
 screen = pg.display.set_mode((350, 525))
 pg.display.set_caption("My 1st game")
-
+clock = pg.time.Clock()
 
 
 class Variables():
@@ -13,12 +13,14 @@ class Variables():
     font_40 = pg.font.Font("Font/UpheavalPro.ttf", 60)
 
     red_dot = pg.image.load("Veci/red_dot.jpg").convert_alpha()
-    red_dot_r = red_dot.get_rect(center=(50, 525/2))
+    red_dot_r = red_dot.get_rect(center=(75, 525/2))
 
     game_active=True
     skore=0
-
-
+    gravitace=0
+    jump_permission = True
+    jump_timer = pg.USEREVENT + 1
+    pg.time.set_timer(jump_timer, 200)
 class Intro_or_Outro():
     
     def intro(font_intro):
@@ -35,21 +37,37 @@ class Intro_or_Outro():
 # class red dot
 class Reddot():
     def gravitace():
-        Variables.red_dot.y +=1
-        if Variables.red_dot.y > 525:
-            Variables.red_dot.y ==525
+            Variables.gravitace += 0.9
+            Variables.red_dot_r.y += Variables.gravitace
+            if Variables.red_dot_r.bottom >= 525:
+                Variables.red_dot_r.bottom =525
+            if Variables.red_dot_r.top <= 0:
+                Variables.red_dot_r.top = 0
     
+    def jump():
+        keys= pg.key.get_pressed()
+        if keys[pg.K_SPACE] and Variables.jump_permission:
+            Variables.gravitace = -10
+            Variables.jump_permission = False
+
+
+    def update():
+        Reddot.gravitace()
+        Reddot.jump()
+
 class main():
     def obraz():
         while True:
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     sys.exit()
-
-                screen.fill("black")
-                screen.blit(Variables.red_dot, Variables.red_dot_r)
-        
+                if event.type == Variables.jump_timer:
+                    Variables.jump_permission = True
+            screen.fill("black")
+            screen.blit(Variables.red_dot, Variables.red_dot_r)
+            Reddot.update()
 
             
             pg.display.update()
-Reddot.obraz()
+            clock.tick(60)
+main.obraz()
